a9e9a96b1a43e8782734aae747eb0cb6482efd48
Geo-hot <55334979+Shrapnel-max@users.noreply.github.com>
2020-10-28 11:31:11 +0200

    Update customers.py
    Modified version of this CRM, we now simply call a function to give us the membership type of a specific customer instead of repeating ourselves

--- customers.py
+++ customers.py
@@ -4,16 +4,13 @@ class Customer:
         self.name = name
         self.email = email
         self.membership_type = membership_type
-        
-c = Customer("Corey", "cdogwework@fastmail.com", "Gold")
-print(c.name, "has a", c.membership_type, "membership type")
 
-c2 = Customer("Brad", "bradleystein@gmail.com", "Bronze")
-print(c2.name, "has a", c2.membership_type, "membership type")
+    def membership(self):
+        return "{} has a {} membership.".format(self.name, self.membership_type)
 
 customers = [Customer("Corey", "cdogwework@fastmail.com", "Gold"),
             Customer("Bradley", "bradleystein@gmail.com", "Bronze")]
 
 # The type pf membership types each customer has
-print(customers[0].name)
-print(customers[1].name, "has a", customers[1].membership_type, "membership_type")
+print(customers[0].membership())
+print(customers[1].membership())
