e9ca13a3d0a72ee2f4114ce0d4b5c010adbebf4d
Geo-hot <55334979+Shrapnel-max@users.noreply.github.com>
2020-10-28 11:46:40 +0200

    Update customers.py
    The CRM is now  setup for an automated email messaging functionality. This is a precursor to that functionality, this is just the ground work

--- customers.py
+++ customers.py
@@ -5,12 +5,20 @@ class Customer:
         self.email = email
         self.membership_type = membership_type
 
-    def membership(self):
+    def membership(self): # the membership type of a customer
         return "{} has a {} membership.".format(self.name, self.membership_type)
 
+    def email_address(self): # makes it easier to just copy and paste a user's address for sending email purposes
+        return "{}'s email address is {}".format(self.name, self.email)
+
+# Store the customers in a list for easy access
 customers = [Customer("Corey", "cdogwework@fastmail.com", "Gold"),
             Customer("Bradley", "bradleystein@gmail.com", "Bronze")]
 
-# The type pf membership types each customer has
+# The type of membership types each customer has
 print(customers[0].membership())
 print(customers[1].membership())
+# Each customer's respective email adsresses
+print(customers[0].email_address())
+print(customers[1].email_address())
+
